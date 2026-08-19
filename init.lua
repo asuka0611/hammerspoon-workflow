6d0dab695990265c1547fc0da5b527dbbf6be3b3
Asuka <miyachiasuka0611@gmail.com>
2026-08-19 15:54:46 +0900

    Enable automatic config reload

--- init.lua
+++ init.lua
@@ -2,6 +2,11 @@
 -- Hammerspoon Entry Point
 -- ============================================
 
+-- System
+-- 設定保存時の自動Reload
+-- 先に読み込んで、以降の設定変更を監視する
+autoReload = require("system.auto_reload")
+
 -- Comic Hotkeys
 comicHotkeys = require("comic.hotkeys")
 
