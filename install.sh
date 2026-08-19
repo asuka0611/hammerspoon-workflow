4fd1cad90785b1091d8335a7cfeb8a7a2aaf8431
Asuka <miyachiasuka0611@gmail.com>
2026-08-19 15:06:43 +0900

    Install window auto focus module

--- install.sh
+++ install.sh
@@ -11,12 +11,13 @@ if [ -d "$DEST" ]; then
   cp -R "$DEST" "$BACKUP"
 fi
 
-mkdir -p "$DEST/common" "$DEST/comic" "$DEST/terminal"
+mkdir -p "$DEST/common" "$DEST/comic" "$DEST/terminal" "$DEST/window"
 
 cp "$ROOT/init.lua" "$DEST/init.lua"
 cp "$ROOT/common/key_sequence.lua" "$DEST/common/key_sequence.lua"
 cp "$ROOT/comic/hotkeys.lua" "$DEST/comic/hotkeys.lua"
 cp "$ROOT/terminal/capture.lua" "$DEST/terminal/capture.lua"
+cp "$ROOT/window/focus.lua" "$DEST/window/focus.lua"
 
 echo "インストール完了"
 echo "Hammerspoonで Reload Config を実行してください。"
